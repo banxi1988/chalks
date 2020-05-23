3de5448554755fdfaea3566c3a1aad78708b71d7
banxi1988 <banxi1988@gmail.com>
2020-05-23 21:04:37 +0800

    refactor: 重写,调整 API 结构,使得使用更流畅

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # chalks
+
 终端粉笔
 
 效果图:
@@ -10,38 +11,43 @@
 #include "chalks/chalks.hpp"
 #include <iostream>
 using chalks::chalk;
-using chalks::chalk_style;
 using std::cout;
 using std::endl;
 
 int main(int argc, char const *argv[]) {
-  cout << chalk("bold red", chalk_style().bold().red()) << "\t"
-       << chalk("dim green", chalk_style().dim().green()) << "\t"
-       << chalk("italic blue", chalk_style().italic().blue()) << "\t"
-       << chalk("underline yellow", chalk_style().underline().yellow()) << "\t"
-       << chalk("bold gray", chalk_style().bold().gray24()) << "\t"
-       << chalk("strike magenta", chalk_style().strike().magenta()) << endl
-       << chalk("color19", chalk_style().color256(196)) << "\t"
-       << chalk("color196", chalk_style().color256(19)) << "\t"
-       << chalk("color26", chalk_style().color256(26)) << "\t"
-       << chalk("color88", chalk_style().color256(88)) << "\t"
-       << chalk("color208", chalk_style().color256(208)) << "\t"
-       << chalk("color100", chalk_style().color256(100)) << "\t"
-       << chalk("color50", chalk_style().color256(50)) << "\t"
-       << chalk("color52", chalk_style().color6x6x6(1, 0, 0)) << "\t" << endl
-       << chalk("light green", chalk_style().green(true)) << "\t"
-       << chalk("light blue", chalk_style().blue(true)) << "\t"
-       << chalk("light yellow", chalk_style().yellow(true)) << "\t"
-       << chalk("light gray", chalk_style().gray24(4)) << "\t"
-       << chalk("light magenta", chalk_style().magenta(true)) << endl
-       << chalk("bg_red", chalk_style().bold().bg_red()) << "\t"
-       << chalk("bg_green", chalk_style().bold().bg_green()) << "\t"
-       << chalk("bg_yellow", chalk_style().bold().bg_yellow()) << "\t"
-       << chalk("bg_blue", chalk_style().bold().bg_blue()) << "\t"
-       << chalk("bg_magenta", chalk_style().bold().bg_magenta()) << "\t"
-       << chalk("bg_cyan", chalk_style().bold().bg_cyan()) << "\t"
-       << chalk("bg_white", chalk_style().bold().bg_white()) << endl;
+  cout << chalks::chalk("[红色]").red().bold().italic().underline()
+       << "这里不要红颜色" << endl;
+  cout << chalk("bold red").bold().red() << "\t"
+       << chalk("dim green").dim().green() << "\t"
+       << chalk("italic blue").italic().blue() << "\t"
+       << chalk("underline yellow").underline().yellow() << "\t"
+       << chalk("bold gray").bold().gray24() << "\t"
+       << chalk("strike magenta").strike().magenta() << endl
+       << chalk("color19").color256(196) << "\t"
+       << chalk("color196").color256(19) << "\t"
+       << chalk("color26").color256(26) << "\t" << chalk("color88").color256(88)
+       << "\t" << chalk("color208").color256(208) << "\t"
+       << chalk("color100").color256(100) << "\t"
+       << chalk("color50").color256(50) << "\t"
+       << chalk("color52").color6x6x6(1, 0, 0) << "\t" << endl
+       << chalk("light green").color(chalks::colors::light_green) << "\t"
+       << chalk("light blue").color(chalks::colors::light_blue) << "\t"
+       << chalk("light yellow").color(chalks::colors::light_yellow) << "\t"
+       << chalk("light gray").gray24(4) << "\t"
+       << chalk("light magenta").color(chalks::colors::light_magenta) << endl
+       << chalk("bg_red").bold().bg_red() << "\t"
+       << chalk("bg_green").bold().bg_green() << "\t"
+       << chalk("bg_yellow").bold().bg_yellow() << "\t"
+       << chalk("bg_blue").bold().bg_blue() << "\t"
+       << chalk("bg_magenta").bold().bg_magenta() << "\t"
+       << chalk("bg_cyan").bold().bg_cyan() << "\t"
+       << chalk("bg_white").bold().bg_white() << endl;
   return 0;
 }
 
+
 ```
+
+### 编译 & 运行
+
+`cd build && cmake .. && make && ./demo/demo`
--- include/chalks/chalks.hpp
+++ include/chalks/chalks.hpp
@@ -1,65 +1,19 @@
 #pragma once
 #include <array>
 #include <cassert>
+#include <ostream>
 #include <sstream>
 #include <string>
 
+namespace chalks {
 using std::stoi;
 using std::string;
-
-namespace chalks {
 
 #define STR(x) #x
 // SGR - Select Graphic Rendition 属于 CSI 序列
 #define SGR(x) "\033[" STR(x) "m"
 
-static const string rgb(int r, int g, int b, bool is_bg = false) {
-  std::ostringstream buf;
-  buf << "\033[" << (is_bg ? 48 : 38) << ";2;" << r << ';' << g << ';' << b
-      << 'm';
-  return buf.str();
-}
-
-static const string color_256(int index, bool is_bg = false) {
-  assert(index >= 0 && index <= 255);
-  std::ostringstream buf;
-  buf << "\033[" << (is_bg ? 48 : 38) << ":5:" << index << 'm';
-  return buf.str();
-}
-
-static const string color_6x6x6(int r, int g, int b) {
-  assert(r >= 0 && r <= 5);
-  assert(g >= 0 && g <= 5);
-  assert(b >= 0 && b <= 5);
-  return color_256(16 + 36 * r + 6 * g + b);
-}
-
-static const string bg_rgb(int r, int g, int b) {
-  return rgb(r, g, b, true);
-}
-
-static const string hex_color(const string &hex, bool is_bg = false) {
-  auto color = (hex[0] == '#') ? hex.substr(1) : hex;
-  int r, g, b;
-  if (color.size() == 3) {
-    r = stoi("0x" + color.substr(0, 1), 0, 16);
-    g = stoi("0x" + color.substr(1, 1), 0, 16);
-    b = stoi("0x" + color.substr(2, 1), 0, 16);
-    return rgb(r * 16 + r, g * 16 + g, b * 16 + b, is_bg);
-  } else if (color.size() == 6) {
-    r = stoi("0x" + color.substr(0, 2), 0, 16);
-    g = stoi("0x" + color.substr(2, 2), 0, 16);
-    b = stoi("0x" + color.substr(4, 2), 0, 16);
-    return rgb(r, g, b, is_bg);
-  }
-  return "";
-}
-
-static inline const string bg_hex_color(const string &hex) {
-  return hex_color(hex, true);
-}
-
-namespace Colors {
+namespace colors {
 constexpr auto black = SGR(30);
 constexpr auto red = SGR(31);
 constexpr auto green = SGR(32);
@@ -68,6 +22,7 @@ constexpr auto blue = SGR(34);
 constexpr auto magenta = SGR(35);
 constexpr auto cyan = SGR(36);
 constexpr auto white = SGR(37);
+
 constexpr auto light_black = SGR(90);
 constexpr auto light_red = SGR(91);
 constexpr auto light_green = SGR(92);
@@ -85,6 +40,7 @@ constexpr auto bg_blue = SGR(44);
 constexpr auto bg_magenta = SGR(45);
 constexpr auto bg_cyan = SGR(46);
 constexpr auto bg_white = SGR(47);
+
 constexpr auto bg_light_black = SGR(100);
 constexpr auto bg_light_red = SGR(101);
 constexpr auto bg_light_green = SGR(102);
@@ -93,264 +49,304 @@ constexpr auto bg_light_blue = SGR(104);
 constexpr auto bg_light_magenta = SGR(105);
 constexpr auto bg_light_cyan = SGR(106);
 constexpr auto bg_light_white = SGR(107);
-}; // namespace Colors
 
-// 二维数组 [char *set,char *reset]
-using _StylePair = std::array<const char *, 2>;
-
-namespace StylePairs {
-constexpr _StylePair bold{SGR(1), SGR(22)};
-constexpr _StylePair dim{SGR(2), SGR(22)};
-constexpr _StylePair italic{SGR(3), SGR(23)};
-constexpr _StylePair underline{SGR(4), SGR(24)};
-constexpr _StylePair inverse{SGR(7), SGR(27)};
-constexpr _StylePair hidden{SGR(8), SGR(28)};
-constexpr _StylePair strike{SGR(9), SGR(29)};
-} // namespace StylePairs
-
-class Style {
-private:
-  // 前景色
-  string _color;
-  // 背景色
-  string _bg_color;
-  bool _underline;
-  bool _dim;
-  bool _strong;
-  bool _bold;
-  bool _strike;
-  bool _inverse;
-  bool _italic;
-
-public:
-  const string color() const noexcept {
-    return _color;
-  }
-  const string bgColor() const noexcept {
-    return _bg_color;
-  }
-
-  Style &color(int r, int g, int b) {
-    _color = rgb(r, g, b);
-    return *this;
-  }
-  Style &clearColor() {
-    _color = "";
-    return *this;
-  }
-  Style &color(const string &hex) {
-    _color = hex_color(hex);
-    return *this;
-  }
-
-  Style &bgColor(int r, int g, int b) {
-    _bg_color = bg_rgb(r, g, b);
-    return *this;
-  }
-
-  Style &bgColor(const string &hex) {
-    _bg_color = bg_hex_color(hex);
-    return *this;
-  }
-  Style &clearBgColor(int r, int g, int b) {
-    _bg_color = "";
-    return *this;
-  }
-
-  Style &underline(bool flag = true) noexcept {
-    _underline = flag;
-    return *this;
-  }
-  Style &dim(bool flag = true) noexcept {
-    _dim = flag;
-    return *this;
-  }
-  Style &strong(bool flag = true) noexcept {
-    _strong = flag;
-    return *this;
-  }
-  Style &bold(bool flag = true) noexcept {
-    _bold = flag;
-    return *this;
-  }
-  Style &strike(bool flag = true) noexcept {
-    _strike = flag;
-    return *this;
-  }
-  Style &inverse(bool flag = true) noexcept {
-    _inverse = flag;
-    return *this;
-  }
-  Style &italic(bool flag = true) noexcept {
-    _italic = flag;
-    return *this;
-  }
-  // 24 阶灰色,1-24 从黑到白
-  Style &gray24(int deep = 12) noexcept {
-    assert(deep >= 1 && deep <= 24);
-    _color = color_256(255 - deep);
-    return *this;
-  }
-  // 24 阶灰色,1-24 从黑到白
-  Style &bg_gray24(int deep = 12) noexcept {
-    assert(deep >= 1 && deep <= 24);
-    _bg_color = color_256(255 - deep);
-    return *this;
-  }
-  //  216 色,16-231  6x6x6: 16+36xr+6xg+b
-  Style &color256(int index) noexcept {
-    _color = color_256(index);
-    return *this;
-  }
-  //  256 色, 除基本色外加 16-231  6x6x6: 16+36xr+6xg+b
-  Style &bg_color256(int index) noexcept {
-    _bg_color = color_256(index);
-    return *this;
-  }
-  //   6x6x6 立方: 16+36xr+6xg+b (0<=r,g,b<=5)
-  Style &color6x6x6(int r, int g, int b) noexcept {
-    _color = color_6x6x6(r, g, b);
-    return *this;
-  }
-  //   6x6x6 立方: 16+36xr+6xg+b (0<=r,g,b<=5)
-  Style &bg_color6x6x6(int r, int g, int b) noexcept {
-    _bg_color = color_6x6x6(r, g, b);
-    return *this;
-  }
-
-  Style &red(bool light = false) noexcept {
-    _color = light ? Colors::light_red : Colors::red;
-    return *this;
-  }
-  Style &bg_red(bool light = false) noexcept {
-    _bg_color = light ? Colors::bg_light_red : Colors::bg_red;
-    return *this;
-  }
-  Style &green(bool light = false) noexcept {
-    _color = light ? Colors::light_green : Colors::green;
-    return *this;
-  }
-  Style &bg_green(bool light = false) noexcept {
-    _bg_color = light ? Colors::bg_light_green : Colors::bg_green;
-    return *this;
-  }
-  Style &yellow(bool light = false) noexcept {
-    _color = light ? Colors::light_yellow : Colors::yellow;
-    return *this;
-  }
-  Style &bg_yellow(bool light = false) noexcept {
-    _bg_color = light ? Colors::bg_light_yellow : Colors::bg_yellow;
-    return *this;
-  }
-  Style &blue(bool light = false) noexcept {
-    _color = light ? Colors::light_blue : Colors::blue;
-    return *this;
-  }
-  Style &bg_blue(bool light = false) noexcept {
-    _bg_color = light ? Colors::bg_light_blue : Colors::bg_blue;
-    return *this;
-  }
-  Style &magenta(bool light = false) noexcept {
-    _color = light ? Colors::light_magenta : Colors::magenta;
-    return *this;
-  }
-  Style &bg_magenta(bool light = false) noexcept {
-    _bg_color = light ? Colors::bg_light_magenta : Colors::bg_magenta;
-    return *this;
-  }
-  Style &cyan(bool light = false) noexcept {
-    _color = light ? Colors::light_cyan : Colors::cyan;
-    return *this;
-  }
-  Style &bg_cyan(bool light = false) noexcept {
-    _bg_color = light ? Colors::light_cyan : Colors::bg_cyan;
-    return *this;
-  }
-  Style &white(bool light = false) noexcept {
-    _color = light ? Colors::light_white : Colors::white;
-    return *this;
-  }
-  Style &bg_white(bool light = false) noexcept {
-    _bg_color = light ? Colors::bg_light_white : Colors::bg_white;
-    return *this;
-  }
-  Style &black(bool light = false) noexcept {
-    _color = light ? Colors::light_black : Colors::black;
-    return *this;
-  }
-  Style &bg_black(bool light = false) noexcept {
-    _bg_color = light ? Colors::bg_light_black : Colors::bg_black;
-    return *this;
-  }
-
-  bool bold() const noexcept {
-    return _bold;
-  }
-  bool strong() const noexcept {
-    return _strong;
-  }
-  bool strike() const noexcept {
-    return _strike;
-  }
-  bool inverse() const noexcept {
-    return _inverse;
-  }
-  bool italic() const noexcept {
-    return _italic;
-  }
-  bool dim() const noexcept {
-    return _dim;
-  }
-  bool underline() const noexcept {
-    return _underline;
-  }
-
-  /**
-   * reset strs
-   */
-  static constexpr auto style_reset_all = SGR(0);
-  static constexpr auto style_reset_fg = SGR(39);
-  static constexpr auto style_reset_bg = SGR(49);
-};
-
-Style chalk_style() noexcept {
-  Style s;
-  return s;
+const string rgb(int r, int g, int b, bool is_bg = false) {
+  std::ostringstream buf;
+  buf << "\033[" << (is_bg ? 48 : 38) << ";2;" << r << ';' << g << ';' << b
+      << 'm';
+  return buf.str();
 }
 
-const string chalk(const string &str, const Style &style) noexcept {
-  string res = str;
-  if (!style.color().empty()) {
-    res = style.color() + res + Style::style_reset_fg;
+const string color_256(int index, bool is_bg = false) {
+  assert(index >= 0 && index <= 255);
+  std::ostringstream buf;
+  buf << "\033[" << (is_bg ? 48 : 38) << ":5:" << index << 'm';
+  return buf.str();
+}
+
+const string color_6x6x6(int r, int g, int b) {
+  assert(r >= 0 && r <= 5);
+  assert(g >= 0 && g <= 5);
+  assert(b >= 0 && b <= 5);
+  return color_256(16 + 36 * r + 6 * g + b);
+}
+
+const string bg_rgb(int r, int g, int b) {
+  return rgb(r, g, b, true);
+}
+
+const string hex_color(const string &hex, bool is_bg = false) {
+  auto color = (hex[0] == '#') ? hex.substr(1) : hex;
+  int r, g, b;
+  if (color.size() == 3) {
+    r = stoi("0x" + color.substr(0, 1), 0, 16);
+    g = stoi("0x" + color.substr(1, 1), 0, 16);
+    b = stoi("0x" + color.substr(2, 1), 0, 16);
+    return rgb(r * 16 + r, g * 16 + g, b * 16 + b, is_bg);
+  } else if (color.size() == 6) {
+    r = stoi("0x" + color.substr(0, 2), 0, 16);
+    g = stoi("0x" + color.substr(2, 2), 0, 16);
+    b = stoi("0x" + color.substr(4, 2), 0, 16);
+    return rgb(r, g, b, is_bg);
   }
-  if (!style.bgColor().empty()) {
-    res = style.bgColor() + res + Style::style_reset_bg;
+  return "";
+}
+
+inline const string bg_hex_color(const string &hex) {
+  return hex_color(hex, true);
+}
+} // namespace colors
+
+namespace textStyles {
+constexpr auto bold = SGR(1);
+constexpr auto dim = SGR(2);
+constexpr auto italic = SGR(3);
+constexpr auto underline = SGR(4);
+constexpr auto inverse = SGR(7);
+constexpr auto hidden = SGR(8);
+constexpr auto strike = SGR(9);
+} // namespace textStyles
+
+namespace resets {
+constexpr auto all = SGR(0);
+constexpr auto foreground = SGR(39);
+constexpr auto fg = SGR(39);
+constexpr auto bg = SGR(49);
+constexpr auto background = SGR(49);
+constexpr auto bold = SGR(22);
+constexpr auto dim = SGR(22);
+constexpr auto italic = SGR(23);
+constexpr auto underline = SGR(24);
+constexpr auto inverse = SGR(27);
+constexpr auto hidden = SGR(28);
+constexpr auto strike = SGR(29);
+} // namespace resets
+
+enum class TextStyle : uint16_t {
+  bold = 1 << 0,
+  dim = 1 << 1,
+  italic = 1 << 2,
+  underline = 1 << 3,
+  inverse = 1 << 4,
+  hidden = 1 << 5,
+  strike = 1 << 6,
+};
+/// make enum flags better
+uint16_t operator+(TextStyle flag) {
+  return static_cast<uint16_t>(flag);
+}
+TextStyle operator|(TextStyle lhs, TextStyle rhs) {
+  return static_cast<TextStyle>((+lhs) | (+rhs));
+}
+TextStyle &operator|=(TextStyle &lhs, TextStyle rhs) {
+  return lhs = lhs | rhs;
+}
+bool operator&(TextStyle lhs, TextStyle rhs) {
+  return ((+lhs) & (+rhs)) != 0;
+}
+
+inline string textStyle(string text, TextStyle styles) {
+  string res = text;
+  if (styles & TextStyle::bold) {
+    res = textStyles::bold + res + resets::bold;
   }
-  if (style.bold() || style.strong()) {
-    auto style = StylePairs::bold;
-    res = style[0] + res + style[1];
+  if (styles & TextStyle::dim) {
+    res = textStyles::dim + res + resets::dim;
   }
-  if (style.dim()) {
-    auto style = StylePairs::dim;
-    res = style[0] + res + style[1];
+  if (styles & TextStyle::italic) {
+    res = textStyles::italic + res + resets::italic;
   }
-  if (style.italic()) {
-    auto style = StylePairs::italic;
-    res = style[0] + res + style[1];
+  if (styles & TextStyle::underline) {
+    res = textStyles::underline + res + resets::underline;
   }
-  if (style.underline()) {
-    auto style = StylePairs::underline;
-    res = style[0] + res + style[1];
+  if (styles & TextStyle::inverse) {
+    res = textStyles::inverse + res + resets::inverse;
   }
-  if (style.inverse()) {
-    auto style = StylePairs::inverse;
-    res = style[0] + res + style[1];
+  if (styles & TextStyle::hidden) {
+    res = textStyles::inverse + res + resets::inverse;
   }
-  if (style.strike()) {
-    auto style = StylePairs::strike;
-    res = style[0] + res + style[1];
+  if (styles & TextStyle::strike) {
+    res = textStyles::strike + res + resets::strike;
   }
   return res;
 }
 
+class Chalk {
+private:
+  string _color;     // 前景色
+  string _bg_color;  // 背景色
+  TextStyle _styles; // 文本样式
+  string _text;      //
+
+public:
+  Chalk(string text) : _text(text) {}
+
+  Chalk &color(int r, int g, int b) {
+    _color = colors::rgb(r, g, b);
+    return *this;
+  }
+  Chalk &clearColor() {
+    _color = "";
+    return *this;
+  }
+  Chalk &color(const string &hex) {
+    _color = colors::hex_color(hex);
+    return *this;
+  }
+
+  Chalk &bgColor(int r, int g, int b) {
+    _bg_color = colors::bg_rgb(r, g, b);
+    return *this;
+  }
+
+  Chalk &bgColor(const string &hex) {
+    _bg_color = colors::bg_hex_color(hex);
+    return *this;
+  }
+  Chalk &clearBgColor(int r, int g, int b) {
+    _bg_color = "";
+    return *this;
+  }
+
+  // 24 阶灰色,1-24 从黑到白
+  Chalk &gray24(int deep = 12) noexcept {
+    assert(deep >= 1 && deep <= 24);
+    _color = colors::color_256(255 - deep);
+    return *this;
+  }
+  /// 24 阶灰色,1-24 从黑到白
+  Chalk &bg_gray24(int deep = 12) noexcept {
+    assert(deep >= 1 && deep <= 24);
+    _bg_color = colors::color_256(255 - deep);
+    return *this;
+  }
+  ///  216 色,16-231  6x6x6: 16+36xr+6xg+b
+  Chalk &color256(int index) noexcept {
+    _color = colors::color_256(index);
+    return *this;
+  }
+  ///  256 色, 除基本色外加 16-231  6x6x6: 16+36xr+6xg+b
+  Chalk &bg_color256(int index) noexcept {
+    _bg_color = colors::color_256(index);
+    return *this;
+  }
+  ///   6x6x6 立方: 16+36xr+6xg+b (0<=r,g,b<=5)
+  Chalk &color6x6x6(int r, int g, int b) noexcept {
+    _color = colors::color_6x6x6(r, g, b);
+    return *this;
+  }
+  ///   6x6x6 立方: 16+36xr+6xg+b (0<=r,g,b<=5)
+  Chalk &bg_color6x6x6(int r, int g, int b) noexcept {
+    _bg_color = colors::color_6x6x6(r, g, b);
+    return *this;
+  }
+
+  Chalk &red() noexcept {
+    this->_color = colors::red;
+    return *this;
+  }
+  Chalk &green() noexcept {
+    this->_color = colors::green;
+    return *this;
+  }
+  Chalk &black() noexcept {
+    this->_color = colors::black;
+    return *this;
+  }
+  Chalk &yellow() noexcept {
+    this->_color = colors::yellow;
+    return *this;
+  }
+  Chalk &blue() noexcept {
+    this->_color = colors::blue;
+    return *this;
+  }
+  Chalk &cyan() noexcept {
+    this->_color = colors::cyan;
+    return *this;
+  }
+  Chalk &magenta() noexcept {
+    this->_color = colors::magenta;
+    return *this;
+  }
+  Chalk &white() noexcept {
+    this->_color = colors::white;
+    return *this;
+  }
+  Chalk &bg_red() noexcept {
+    _bg_color = colors::bg_red;
+    return *this;
+  }
+  Chalk &bg_green() noexcept {
+    _bg_color = colors::bg_green;
+    return *this;
+  }
+  Chalk &bg_blue() noexcept {
+    _bg_color = colors::bg_blue;
+    return *this;
+  }
+  Chalk &bg_black() noexcept {
+    _bg_color = colors::bg_black;
+    return *this;
+  }
+  Chalk &bg_yellow() noexcept {
+    _bg_color = colors::bg_yellow;
+    return *this;
+  }
+  Chalk &bg_cyan() noexcept {
+    _bg_color = colors::bg_cyan;
+    return *this;
+  }
+  Chalk &bg_magenta() noexcept {
+    _bg_color = colors::bg_magenta;
+    return *this;
+  }
+  Chalk &bg_white() noexcept {
+    _bg_color = colors::bg_white;
+    return *this;
+  }
+  Chalk &bold() noexcept {
+    this->_styles |= TextStyle::bold;
+    return *this;
+  }
+  Chalk &italic() noexcept {
+    this->_styles |= TextStyle::italic;
+    return *this;
+  }
+  Chalk &underline() noexcept {
+    this->_styles |= TextStyle::underline;
+    return *this;
+  }
+  Chalk &dim() noexcept {
+    this->_styles |= TextStyle::dim;
+    return *this;
+  }
+  Chalk &hidden() noexcept {
+    this->_styles |= TextStyle::hidden;
+    return *this;
+  }
+  Chalk &strike() noexcept {
+    this->_styles |= TextStyle::strike;
+    return *this;
+  }
+
+  string build() const noexcept {
+    string text = _color.empty() ? _text : _color + _text + resets::fg;
+    text = _bg_color.empty() ? text : _bg_color + text + resets::bg;
+    return textStyle(text, _styles);
+  }
+
+  string styled_text() const noexcept {
+    return build();
+  }
+};
+
+inline Chalk chalk(string text) {
+  Chalk c(text);
+  return c;
+}
+std::ostream &operator<<(std::ostream &os, const Chalk &c) {
+  os << c.build();
+  return os;
+}
 } // namespace chalks
